72fc345d7c600f2d3a2c958a9c386c3c5e1a52a3
chibimagic <github@chibimagic.org>
2012-08-20 17:04:58 -0700

    Update MockElement to mock all the assertions that WebElement has

--- WebDriver/MockElement.php
+++ WebDriver/MockElement.php
@@ -7,10 +7,19 @@ class WebDriver_MockElement extends WebDriver_WebElement {
   public function assert_disabled() { return true; }
   public function assert_selected() { return true; }
   public function assert_not_selected() { return true; }
+  public function assert_contains_element($child_locator) { return true; }
+  public function assert_does_not_contain_element($child_locator) { return true; }
+  public function assert_next_element_count($expected_count, $locator) { return true; }
   public function assert_text($expected_text) { return true; }
+  public function assert_text_reload($expected_text) { return true; }
   public function assert_text_contains($expected_needle) { return true; }
   public function assert_text_does_not_contain($expected_missing_needle) { return true; }
   public function assert_value($expected_value) { return true; }
+  public function assert_attribute_value($attribute_name, $expected_value) { return true; }
+  public function assert_attribute_contains($attribute_name, $expected_needle) { return true; }
+  public function assert_attribute_does_not_contain($attribute_name, $expected_missing_needle) { return true; }
+  public function assert_css_value($property_name, $expected_value, $canonicalize_colors = true) { return true; }
   public function assert_option_count($expected_count) { return true; }
+  public function assert_selected_count($expected_count) { return true; }
   public function assert_contains_label($expected_label) { return true; }
 }
